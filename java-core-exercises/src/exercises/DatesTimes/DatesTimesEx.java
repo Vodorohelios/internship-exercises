fc9c7125abcbd2e560e9677d631a5f7b8ea7cefd
Jlobici Alexandr <sunrise.battle@gmail.com>
2017-09-18 21:39:21 +0300

    Added exercises (Dates and Times).

---
+++ java-core-exercises/src/exercises/DatesTimes/DatesTimesEx.java
@@ -0,0 +1,108 @@
+package exercises.DatesTimes;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class DatesTimesEx {
+    public static void main(String[] args) {
+        // Java 8 date & time
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+
+        // Creating dates and times
+
+        // local date
+        LocalDate date1 = LocalDate.of(2015, Month.JANUARY, 20);
+        LocalDate date2 = LocalDate.of(2015, 1, 20);
+
+        // local time
+        LocalTime time1 = LocalTime.of(6, 15); // hour and minute
+        LocalTime time2 = LocalTime.of(6, 15, 30); // + seconds
+        LocalTime time3 = LocalTime.of(6, 15, 30, 200); // + nanoseconds
+
+        // local date and time combo
+        LocalDateTime dateTime1 = LocalDateTime.of(2015, Month.JANUARY, 20, 6, 15, 30);
+        LocalDateTime dateTime2 = LocalDateTime.of(date1, time1);
+
+//        LocalDate d = new LocalDate(); // DOES NOT COMPILE
+
+        // Manipulating dates and times
+
+        // adding date
+        LocalDate date = LocalDate.of(2014, Month.JANUARY, 20);
+        System.out.println(date); // 2014-01-20
+        date = date.plusDays(2);
+        System.out.println(date); // 2014-01-22
+        date = date.plusWeeks(1);
+        System.out.println(date); // 2014-01-29
+        date = date.plusMonths(1);
+        System.out.println(date); // 2014-02-28
+        date = date.plusYears(5);
+        System.out.println(date); // 2019-02-28
+
+        // subtracting date
+        date = LocalDate.of(2020, Month.JANUARY, 20);
+        LocalTime time = LocalTime.of(5, 15);
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+        System.out.println(dateTime); // 2020-01-20T05:15
+        dateTime = dateTime.minusDays(1);
+        System.out.println(dateTime); // 2020-01-19T05:15
+        dateTime = dateTime.minusHours(10);
+        System.out.println(dateTime); // 2020-01-18T19:15
+        dateTime = dateTime.minusSeconds(30);
+        System.out.println(dateTime); // 2020-01-18T19:14:30
+
+        // chaining
+        date = LocalDate.of(2020, Month.JANUARY, 20);
+        time = LocalTime.of(5, 15);
+        dateTime = LocalDateTime.of(date2, time)
+                .minusDays(1).minusHours(10).minusSeconds(30);
+
+        // Periods
+
+        Period annually = Period.ofYears(1); // every 1 year
+        Period quarterly = Period.ofMonths(3); // every 3 months
+        Period everyThreeWeeks = Period.ofWeeks(3); // every 3 weeks
+        Period everyOtherDay = Period.ofDays(2); // every 2 days
+        Period everyYearAndAWeek = Period.of(1, 0, 7); // every year and 7 days
+
+        date = LocalDate.of(2015, 1, 20);
+        time = LocalTime.of(6, 15);
+        dateTime = LocalDateTime.of(date, time);
+        Period period = Period.ofMonths(1);
+        System.out.println(date.plus(period)); // 2015-02-20
+        System.out.println(dateTime.plus(period)); // 2015-02-20T06:15
+//        System.out.println(time.plus(period)); // UnsupportedTemporalTypeException
+
+        // Formatting dates and times
+
+        date = LocalDate.of(2020, Month.JANUARY, 20);
+        time = LocalTime.of(11, 12, 34);
+        dateTime = LocalDateTime.of(date, time);
+        System.out.println(date
+                .format(DateTimeFormatter.ISO_LOCAL_DATE));
+        System.out.println(time.format(DateTimeFormatter.ISO_LOCAL_TIME));
+        System.out.println(dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+        DateTimeFormatter shortDateTime =
+                DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        System.out.println(shortDateTime.format(dateTime)); // 1/20/20
+        System.out.println(shortDateTime.format(date)); // 1/20/20
+//        System.out.println(
+//                shortDateTime.format(time)); // UnsupportedTemporalTypeException
+
+        // custom formatter
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("MMMM dd, yyyy, hh:mm");
+        System.out.println(dateTime.format(f)); // January 20, 2020, 11:12
+
+        // Parsing dates and times
+
+        f = DateTimeFormatter.ofPattern("MM dd yyyy");
+        date = LocalDate.parse("01 02 2015", f);
+        time = LocalTime.parse("11:22");
+        System.out.println(date); // 2015-01-02
+        System.out.println(time); // 11:22
+    }
+}
