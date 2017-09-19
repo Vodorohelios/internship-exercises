92779eb80253386c0052a2bd876e1ceaf6f970cf
Alexandr Jlobici <ajlobici@endava.com>
2017-09-19 13:24:39 +0300

    Added exercises (Access modifiers).

---
+++ java-core-exercises/src/exercises/accessmodifiers/pond/shore/Bird.java
@@ -0,0 +1,10 @@
+package exercises.accessmodifiers.pond.shore;
+
+public class Bird {
+    protected String text = "floating"; // protected access
+
+    protected void floatInWater() { // protected access
+        System.out.println(text);
+    }
+}
+
