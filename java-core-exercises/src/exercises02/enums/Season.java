d471095a29b91542cd7ba848d0dfdec7b3c02cbe
Alexandr Jlobici <ajlobici@endava.com>
2017-09-26 11:31:05 +0300

    Added exercises

---
+++ java-core-exercises/src/exercises02/enums/Season.java
@@ -0,0 +1,19 @@
+package exercises02.enums;
+
+public enum Season {
+    WINTER("Low"), SPRING("Medium"), SUMMER("High"), FALL("Medium");
+    private String expectedVisitors;
+
+    private Season(String expectedVisitors) {
+        this.expectedVisitors = expectedVisitors;
+    }
+
+    public void printExpectedVisitors() {
+        System.out.println(expectedVisitors);
+    }
+
+    public static void main(String[] args) {
+        Season.SUMMER.printExpectedVisitors();
+    }
+}
+
