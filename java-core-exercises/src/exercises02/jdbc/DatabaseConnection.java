4530155c42e8d164de69d76952500275a2a611bb
Alexandr Jlobici <ajlobici@endava.com>
2017-09-27 13:57:37 +0300

    Added changes to exercises (jdbc)

--- java-core-exercises/src/exercises02/jdbc/DatabaseConnection.java
+++ java-core-exercises/src/exercises02/jdbc/DatabaseConnection.java
@@ -4,12 +4,23 @@ import java.sql.*;
 
 public class DatabaseConnection {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost/zoo?user=root&password=1234";
+        String url = "jdbc:mysql://localhost/zoo?user=root&password=root";
         try (Connection conn = DriverManager.getConnection(url);
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("select name from animal")) {
-            while (rs.next())
-                System.out.println(rs.getString(1));
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select name from animal")) {
+//            while (rs.next())
+//                System.out.println(rs.getString(1));
+
+
+            int count = stmt.executeUpdate(
+                    "update animal set name = 'New Elsa' where name = 'Elsa'");
+
+            System.out.println();
+
+            ResultSet rs2 = stmt.executeQuery("select name from animal");
+            while (rs2.next()) {
+                System.out.println(rs2.getString(1));
+            }
         }
     }
 }
--- java-core-exercises/src/exercises02/jdbc/SetupMySqlDatabase.java
+++ java-core-exercises/src/exercises02/jdbc/SetupMySqlDatabase.java
@@ -6,7 +6,7 @@ import java.sql.Statement;
 
 public class SetupMySqlDatabase {
     public static void main(String[] args) throws Exception {
-        String url = "jdbc:mysql://localhost/zoo?user=root&password=1234";
+        String url = "jdbc:mysql://localhost/zoo?user=root&password=root";
         try (Connection conn = DriverManager.getConnection(url);
              Statement stmt = conn.createStatement()) {
             stmt.executeUpdate("CREATE TABLE species ("
