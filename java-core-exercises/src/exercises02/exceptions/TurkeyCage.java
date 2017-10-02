4627acd12022d3461edd0d3aaacda1b1f83afb4a
Jlobici Alexandr <sunrise.battle@gmail.com>
2017-10-02 22:47:55 +0300

    Added exercises (Exceptions)

---
+++ java-core-exercises/src/exercises02/exceptions/TurkeyCage.java
@@ -0,0 +1,13 @@
+package exercises02.exceptions;
+
+public class TurkeyCage implements AutoCloseable {
+    public void close() {
+        System.out.println("Close gate");
+    }
+
+    public static void main(String[] args) {
+        try (TurkeyCage t = new TurkeyCage()) {
+            System.out.println("put turkeys in");
+        }
+    }
+}
