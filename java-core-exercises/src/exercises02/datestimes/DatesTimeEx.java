9c5334613917f86bb54447b4aa5a5b8e7849227c
Alexandr Jlobici <ajlobici@endava.com>
2017-10-03 10:53:11 +0300

    Added exercises (DatesTimeEx)

---
+++ java-core-exercises/src/exercises02/datestimes/DatesTimeEx.java
@@ -0,0 +1,10 @@
+package exercises02.datestimes;
+
+import java.util.Locale;
+
+public class DatesTimeEx {
+    public static void main(String[] args) {
+        Locale locale = Locale.getDefault();
+        System.out.println(locale);
+    }
+}
